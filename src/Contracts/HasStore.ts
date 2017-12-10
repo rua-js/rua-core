fdf53feb579b11522a91454ddff672ec589c2d7c
copydog <775150@qq.com>
2017-12-10 23:20:22 +0800

    update Store design

--- src/Contracts/HasStore.ts
+++ src/Contracts/HasStore.ts
@@ -2,4 +2,6 @@ import { Store } from '../Types/index'
 
 export default interface HasStore {
   store: Store,
+  saveStore: (store: Store) => Store
+  resetStore: () => Store
 }
--- src/Types/Store.ts
+++ src/Types/Store.ts
@@ -1,3 +1,3 @@
 export default interface Store {
-  [key: string]: any,
+  [key: string]: any
 }
