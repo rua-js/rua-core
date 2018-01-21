44a49e3928d6bea4e9d56c92d99d6ada7cae408e
copydog <775150@qq.com>
2018-01-21 19:52:47 +0800

    register NullableObject

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,9 +1,11 @@
 import Store from './Store'
 import Nullable from './Nullable'
 import AnyObject from './AnyObject'
+import NullableObject from './NullableObject'
 
 export {
   Store,
   AnyObject,
   Nullable,
+  NullableObject,
 }
