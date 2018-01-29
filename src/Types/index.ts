7ce0c3fc41469aa1a57bb96d8b9d941ec002e704
copydog <775150@qq.com>
2018-01-29 12:36:37 +0800

    exporting Types

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,13 +1,38 @@
-import Store from './Store'
+// Primitives
 import Nullable from './Nullable'
+import Nilable from './Nilable'
+import Undefinable from './Undefinable'
+// Any
 import AnyObject from './AnyObject'
+import AnyBoolean from './AnyBoolean'
+// Nullable
 import NullableObject from './NullableObject'
 import NullableBoolean from './NullableBoolean'
+// Undefinable
+import UndefinableObject from './UndefinableObject'
+import UndefinableBoolean from './UndefinableBoolean'
+// Nilable
+import NilableObject from './NilableObject'
+import NilableBoolean from './NilableBoolean'
+// Other
+import Store from './Store'
 
 export {
   Store,
-  AnyObject,
+  // Primitives
   Nullable,
+  Nilable,
+  Undefinable,
+  // Any
+  AnyObject,
+  AnyBoolean,
+  // Nullable
   NullableObject,
   NullableBoolean,
+  // Undefinable
+  UndefinableObject,
+  UndefinableBoolean,
+  // Nilable
+  NilableObject,
+  NilableBoolean,
 }
