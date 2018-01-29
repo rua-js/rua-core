d75963fbbc138d3abf8f907a960f6078ca5d6b46
copydog <775150@qq.com>
2018-01-29 13:07:15 +0800

    AnyData

---
+++ src/Types/AnyData.ts
@@ -0,0 +1,7 @@
+import AnyBoolean from './AnyBoolean'
+import AnyObject from './AnyObject'
+import Nilable from './Nilable'
+
+type AnyData = AnyBoolean | AnyObject | string | Nilable | number
+
+export default AnyData
