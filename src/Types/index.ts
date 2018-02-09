cb1e813d3bfcc2810e2b7d981f547f0033e40bcb
copydog <775150@qq.com>
2018-02-09 17:43:28 +0800

    Type Array

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -6,15 +6,19 @@ import Undefinable from './Undefinable'
 import AnyObject from './AnyObject'
 import AnyBoolean from './AnyBoolean'
 import AnyData from './AnyData'
+import AnyArray from './AnyArray'
 // Nullable
 import NullableObject from './NullableObject'
 import NullableBoolean from './NullableBoolean'
+import NullableArray from './NullableArray'
 // Undefinable
 import UndefinableObject from './UndefinableObject'
 import UndefinableBoolean from './UndefinableBoolean'
+import UndefinableArray from './UndefinableArray'
 // Nilable
 import NilableObject from './NilableObject'
 import NilableBoolean from './NilableBoolean'
+import NilableArray from './NilableArray'
 // Other
 import Store from './Store'
 
@@ -28,13 +32,17 @@ export {
   AnyObject,
   AnyBoolean,
   AnyData,
+  AnyArray,
   // Nullable
   NullableObject,
   NullableBoolean,
+  NullableArray,
   // Undefinable
   UndefinableObject,
   UndefinableBoolean,
+  UndefinableArray,
   // Nilable
   NilableObject,
   NilableBoolean,
+  NilableArray,
 }
